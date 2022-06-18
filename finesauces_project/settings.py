5bf8edcc09955d7ebb5faa83e6ffcd85dc258ec0
leandroReyes <leandro.reyes.castillo@gmail.com>
2022-06-18 12:45:44 -0400

    respaldo 18-06-2022

--- finesauces_project/settings.py
+++ finesauces_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 try:
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'listings.apps.ListingsConfig'
 ]
 
 MIDDLEWARE = [
@@ -111,3 +112,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
